aafd1b91d86f5021303f5f4f0824167eef3b471e
Alejandro Mejia <alejandromejia2013.27@gmail.com>
2021-09-19 16:18:25 -0300

    Adding book with scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_030241) do
+ActiveRecord::Schema.define(version: 2021_09_19_190627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "isbn"
+    t.integer "page_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
